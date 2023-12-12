d91cab8b3645f56a5fd5b6315db4fa2872eb77fe
Melissa J. Chin <Melissa@Melissas-MacBook-Air.local>
2023-12-12 16:18:22 -0600

    adding updated files

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import {useState} from "react";
 import ContactRow from './ContactRow'
-
-const dummyContacts = [
-  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-];
+import {useEffect} from 'react'
 
 
 export default function ContactList() {
-  const [contacts, setContacts] = useState(dummyContacts)
+  const [contacts, setContacts] = useState([])
+  console.log(contacts)
+  useEffect(()=>{
+    async function fetchContacts(){
+      try {
+        const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users")
+        const result = await response.json(); 
+        setContacts(result); 
+        console.log(result)
+   
+      } catch(error) {
+        console.log(error); 
+      }
+    }
+    fetchContacts()
+  },[])
 
-  console.log("Contacts: ", contacts)
+  console.log(contacts)
     return ( 
         <table>
           <thead>
@@ -26,7 +36,7 @@ export default function ContactList() {
               <td>Email</td>
               <td>Phone</td>
             </tr>
-              {dummyContacts.map((contact) => {
+              {contacts.map((contact) => {
                 return <ContactRow key={contact.id} contact={contact} />;
               })}
           </tbody>
--- src/components/ContactRow.jsx
+++ src/components/ContactRow.jsx
@@ -1,9 +1,9 @@
 export default function ContactRow({ contact }) {
-    return (
-      <tr>
-        <td>{contact.name}</td>
-        <td>{contact.email}</td>
-        <td>{contact.phone}</td>
-      </tr>
-    );
-  }
+  return (
+    <tr>
+      <td>{contact.name}</td>
+      <td>{contact.email}</td>
+      <td>{contact.phone}</td>
+    </tr>
+  );
+}
